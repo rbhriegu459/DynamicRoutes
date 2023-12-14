b36a56d01b89cd9bd2069e67574718c1c76fd926
rbhriegu459 <rbhriegu459@gmail.com>
2023-12-15 00:19:29 +0530

    Change3

--- controllers/shop.js
+++ controllers/shop.js
@@ -40,11 +40,12 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req,res,next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product =>{
+  Product.findBy(prodId, product =>{
     Cart.addProduct(prodId, product.price);
   });
   res.redirect('/cart');
 };
+
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
